3e5ece30a04ed58159415dfc2bfd9949657a2c3b
Thanapon Dongphuyaw <thanapon.dongphuyaw@gmail.com>
2024-08-30 16:26:28 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -38,7 +38,6 @@ console.clear();
 gsap.registerPlugin(ScrollTrigger);
 
 window.addEventListener("load", () => {
-    
   gsap
     .timeline({
       scrollTrigger: {
@@ -50,12 +49,21 @@ window.addEventListener("load", () => {
         markers: true
       }
     })
-    .to(".image-container", {
-      scale: 5,
+    .to("img", {
+      scale: 2,
       z: 350,
       transformOrigin: "center center",
       ease: "power1.inOut"
     })
+    .to(
+      ".section.hero",
+      {
+        scale: 1.1,
+        transformOrigin: "center center",
+        ease: "power1.inOut"
+      },
+      "<"
+    );
 });
 
 document.querySelector('.myButton').addEventListener('click', function() {
